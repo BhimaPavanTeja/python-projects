687b0d8fe795aff6fa2ff3f9a8721b623a167905
Bhima Pavan Teja <124360829+BhimaPavanTeja@users.noreply.github.com>
2024-04-05 23:01:02 +0530

    Create turtleGraphics02.py

---
+++ turtleGraphics02.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.color("yellow")
+turtle.bgcolor("black")
+for i in range(7):
+    turtle.circle(50)
+    turtle.left(50)
+
+turtle.color("blue")
+for i in range(7):
+    turtle.circle(70)
+    turtle.left(50)
+
+turtle.color("orange")
+for i in range(7):
+    turtle.circle(100)
+    turtle.left(50)
+
+turtle.done()
